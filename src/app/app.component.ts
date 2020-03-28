d970fa8f767df5b15bf19bb7f209887892fa30b5
cleiton016 <cleitonluiz2014@gmail.com>
2020-03-28 01:16:01 -0300

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  pessoa = null;
+  lista = [];
+  adicionar() {
+    if (this.pessoa != null) {
+      this.lista.push(this.pessoa);
+    }
+    this.pessoa = null;
+  }
+  apagar(key: number) {
+    this.lista.splice(key, 1);
+  }
+
+}
